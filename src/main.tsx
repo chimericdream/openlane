deaedfff976d69379c18d8e76b8a401f832770a8
Bill Parrott <bill@chimericdream.com>
2024-01-23 19:44:37 -0600

    chore: add deps and remove unneeded files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './app.tsx';
-import './index.css';
+
+import './assets.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
